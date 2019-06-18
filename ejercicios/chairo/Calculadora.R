f780db8529d0fa26692d20d9c807a6f7d2a2fb47
dador33 <dador33@users.noreply.github.com>
2019-06-18 14:39:05 -0500

    18062019

---
+++ ejercicios/chairo/Calculadora.R
@@ -0,0 +1,64 @@
+2+3
+
+5/2
+s
+#obtener la parte entera
+5%/%3
+
+3%/%5
+
+# Para obtener el residuo
+725%%7
+
+#Funciones
+sqrt(9)
+
+log(exp(1))
+
+log (1000,10)
+
+factorial(3)
+
+choose(4,2)
+
+choose(5,2)
+
+sin(2)
+
+#Para los sin y cos se debe relizar esta conversion
+cos(60*pi/180)
+
+sin(60*pi/180)
+
+?sin
+
+tan(pi) # -1.224647e-16 por su valor la tendencia es 0
+
+tan(pi/2) #Numero relativamente infinito
+
+asin(0.8660254)*180/pi #resultado es 60 grados pero teniendo en cuenta la conversion
+
+#Redondeos
+sqrt(2)
+
+print(sqrt(2),10) #Los primeros 10 digitos del resultado
+
+round(sqrt(2),3) #Trae parte entera con los 3 decimales.
+
+floor(sqrt(2)) #Redondeo al piso 1.414214-> 1
+
+ceiling(sqrt(2)) #Redondeo a la cabeza piso -> 2
+
+trunc(sqrt(2)) #tomar la parte entera
+
+
+print(2^50,2)
+
+round(1.255,1)
+
+round(1.25,1)
+
+round(sqrt(2),digits = 3) #Especificar la cantidad de cifras decimales.
+
+
+ 
---
+++ ejercicios/chairo/install.R
@@ -0,0 +1,12 @@
+#Tener presente que se debe instalar Rtools la version recomendada.
+install.packages("tidyverse",dep = TRUE)
+library("tidyverse")
+
+install.packages("magic", dep=TRUE)
+
+library(magic)
+
+magic(6)
+
+installed.packages()
+
